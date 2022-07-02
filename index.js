c526abbc91b9aa4ace20c86130a36fb0138df9b1
Craspar <89880549+Craspar@users.noreply.github.com>
2022-07-02 16:04:50 -0500

    Initial Commit

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const Discord = require("discord.js")
 
+const welcomeID = "992858522897358891"
+
 require("dotenv").config()
 
 const client = new Discord.Client({
     intents: [
         "GUILDS",
-        "GUILD_MESSAGES"
+        "GUILD_MESSAGES",
+        "GUILD_MEMBERS"
     ]
 })
 
@@ -14,9 +17,19 @@ client.on("ready", () => {
 })
 
 client.on("messageCreate", (message) => {
-    if (message.content == "hi"){
-        message.reply("Hello World!")
+    if (message.content == "!class"){
+        message.reply("This class is ACSG-545 Software Engineering!")
     }
+})
+
+client.on("messageCreate", (message) => {
+    if (message.content == "!prof"){
+        message.reply("The Professor of this class is Imad Al-Saeed, Email: alsaeed@sxu.edu")
+    }
+})
+
+client.on("guildMemberAdd", (member) => {
+    member.guild.channels.cache.get(welcomeID).send(`<@${member.id}> Welcome to the test server for Group 4!`)
 })
 
 client.login(process.env.TOKEN)
