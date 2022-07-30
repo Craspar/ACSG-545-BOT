436b321a42a7d5c4a51a58d455439924b00c206b
Craspar <89880549+Craspar@users.noreply.github.com>
2022-07-30 12:13:22 -0500

    Update index.js

--- index.js
+++ index.js
@@ -10,39 +10,27 @@ const client = new Discord.Client({
         "GUILD_MESSAGES",
         "GUILD_MEMBERS"
     ]
-});
+})
 
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}`)
-
-    var testChannel = client.channels.find(channel => channel.id === '1002978821399658617');
-
-    setInterval(() => {
-        testChannel.send("This message was sent on a timer!");
-    }, 5000);
-});
+})
 
 client.on("messageCreate", (message) => {
     if (message.content == "!class"){
         message.reply("This class is ACSG-545 Software Engineering!")
     }
-});
+})
 
 client.on("messageCreate", (message) => {
     if (message.content == "!prof"){
         message.reply("The Professor of this class is Imad Al-Saeed, Email: alsaeed@sxu.edu")
     }
-});
-
-client.on("messageCreate", (message) => {
-    if (message.content == "!members"){
-        message.reply("The members of Group 4 are Christopher Beck, Nicholas Josten, and Petero Kwizera")
-    }
-});
+})
 
 client.on("guildMemberAdd", (member) => {
     member.guild.channels.cache.get(welcomeID).send(`<@${member.id}> Welcome to the test server for Group 4!`)
-});
+})
 
 client.on('message', message => {
     if (message.content == '!ping')
@@ -51,4 +39,4 @@ client.on('message', message => {
     }
 });
 
-client.login(process.env.TOKEN);
+client.login(process.env.TOKEN)
