3199618479aac90307a46efd3b92c1e15ef93a87
Craspar <89880549+Craspar@users.noreply.github.com>
2022-07-30 21:11:42 -0500

    Update index.js

--- index.js
+++ index.js
@@ -67,6 +67,13 @@ client.on('message', message => {
     }
 });
 
+client.on('message', message => {
+    if (message.content == '!help')
+    {
+        message.reply('Commands List:\n!class => Returns the corresponding class for this server.\n!prof => Returns information regarding the class instructor.\n!ping performs a basic ping test, returning "pong" if the bot is online.\n!rules returns a list of current server rules.\n!help returns this list of coommands.');
+    }
+});
+
 var id;
 
 var blackList = ['test1', 'test2', 'test3'];
